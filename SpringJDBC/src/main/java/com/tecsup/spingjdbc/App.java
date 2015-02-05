130dab46f6dba95330f63c0766241ebef2ab12fb
usuario <usuario@172.16.64.47>
2015-02-05 15:06:15 -0500

    cambio 03:06

--- SpringJDBC/src/main/java/com/tecsup/spingjdbc/App.java
+++ SpringJDBC/src/main/java/com/tecsup/spingjdbc/App.java
@@ -9,20 +9,36 @@ import com.tecsup.spingjdbc.dao.ProgramaDao;
 import com.tecsup.spingjdbc.model.Programa;
 
 public class App {
-	
-	
-public static void main(String[] args) {
-    ApplicationContext cntx = new ClassPathXmlApplicationContext("app.xml");
-	App.doList(cntx);
-}
 
-public static void doList(ApplicationContext cntx) {
-	ProgramaDao dao = (ProgramaDao) cntx.getBean("programaDao");
-	List<Programa> programas = dao.list();
-	for(Programa programa : programas){
-		System.out.println(programa.getNombre()+programa.getId());
+	public static void main(String[] args) {
+		ApplicationContext cntx = new ClassPathXmlApplicationContext("app.xml");
+		//App.doDelete(cntx, 1);
+		App.doSave(cntx);
+		App.doList(cntx);
+	}
+
+	public static void doList(ApplicationContext cntx) {
+		ProgramaDao dao = (ProgramaDao) cntx.getBean("programaDao");
+		List<Programa> programas = dao.list();
+		for (Programa programa : programas) {
+			System.out.println(programa.getNombre() + programa.getId());
+		}
+
+	}
+
+	public static void doDelete(ApplicationContext cntx, long id) {
+		ProgramaDao dao = (ProgramaDao) cntx.getBean("programaDao");
+		dao.delete(id);
+
 	}
 	
-	
-}
+	public static void doSave (ApplicationContext cntx){
+		ProgramaDao dao = (ProgramaDao)cntx.getBean("programaDao");
+		Programa pr = new Programa();
+		
+		pr.setCodigo("77");
+		pr.setDescripcion("caso77");
+		pr.setNombre("caso77");
+		dao.save(pr);
+	}
 }
